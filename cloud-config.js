933f2008cbd491d0b1e5f60895d0cd10db043bca
Studio Master <studiomaster794-prog@users.noreply.github.com>
2026-08-28 18:36:05 -0300

    chore: liga a nuvem do escritorio no aplicativo

--- cloud-config.js
+++ cloud-config.js
@@ -3,6 +3,6 @@
    A chave anon é pensada para ficar no aplicativo; os dados
    ficam protegidos pelo código do escritório. */
 window.AGENDA_CLOUD = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://dfbjrpokaspdtjkonubg.supabase.co',
+  supabaseAnonKey: 'sb_publishable_FVh5az2pjXg3zF7Hs7iywA_PTBaQaSq',
 };
